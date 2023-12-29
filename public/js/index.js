b9e27bd774fde30437068623bb5d50ce79aac66c
sushantskb <91365425+sushantskb@users.noreply.github.com>
2023-12-29 23:30:38 +0530

    added the header and some minor cdns

---
+++ public/js/index.js
@@ -0,0 +1,35 @@
+"use strict"
+
+// add event on element
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length !== undefined) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+};
+
+
+
+
+//navbar toggle
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const navToggler = document.querySelector("[data-nav-toggler]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  this.classList.toggle("active");
+}
+
+addEventOnElem(navToggler, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  navToggler.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
