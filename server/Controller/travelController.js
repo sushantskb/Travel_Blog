fa361ded8fc0cd8ceff769f46e85dfcdd047298d
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-01 14:23:50 +0530

    added the route, Controller and all posts page

--- server/Controller/travelController.js
+++ server/Controller/travelController.js
@@ -1,3 +1,7 @@
 exports.homepage = async (req, res) => {
     res.render("index");
 }
+
+exports.allposts = async(req, res) => {
+    res.render("allpost");
+}
--- server/Routes/travelRoutes.js
+++ server/Routes/travelRoutes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 const travelController = require("../Controller/travelController");
 router.get("/", travelController.homepage);
+router.get("/all-post", travelController.allposts);
 
 module.exports = router;
