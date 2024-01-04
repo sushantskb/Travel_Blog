8766d4f7c112e9e8572695ced97c55efd9557386
sushantskb <91365425+sushantskb@users.noreply.github.com>
2024-01-04 22:07:14 +0530

    dynamically randered data for the travel section

--- server/Controller/blogController.js
+++ server/Controller/blogController.js
@@ -1,13 +1,15 @@
 const Blog = require("../Models/Blog");
+const Feature = require("../Models/Feature");
 
 exports.homepage = async (req, res) => {
     try {
         const limitNumber = 4;
-        const featured = await Blog.find({}).sort({_id: -1}).limit(limitNumber);
+        const featured = await Feature.find({}).limit(limitNumber);
+        const travel = await Blog.find({}).sort({_id: -1}).limit(5);
 
-        const blogs = { featured };
+        const blogs = { featured, travel };
         console.log(blogs);
-        res.render("index", {blogs});
+        res.render("index", { blogs });
 
     } catch (error) {
         res.status(500).send({message: error.message || "Error Ocuured"});
@@ -31,45 +33,48 @@ exports.contact = async (req, res) => {
   return res.render("contact");
 };
 
-// async function insertDummyData() {
-//   try {
-//     await Blog.insertMany([
-//       {
-//         title: "The old city | Places, Quebec city, Canada travel",
-//         blog_image: "featured-1.jpg",
-//         author_img: "author-1.jpg",
-//         category: "Travel",
-//         content:
-//           "Vestibulum vehicula dui venenatis neque tempor, accumsan iaculis sapien ornare. Sed at ante porta, ullamcorper massa eu, ullamcorper sapien. Donec pretium tortor augue. Integer egestas ut tellus sed pretium. Nullam tristique augue ut mattis vulputate. Duis et lorem in odio ultricies porttitor.",
-//       },
-//       {
-//         title:
-//           "Samsung Galaxy S24 Ultra to Shatter Brightness Records and Have Huge Bezels",
-//         blog_image: "featured-2.jpg",
-//         author_img: "author-3.jpg",
-//         category: "Tech",
-//         content:
-//           "Quibus autem in rebus tanta obscuratio non fit, fieri tamen potest, ut id ipsum, quod interest, non sit magnum. Ita fit ut, quanta differentia est in principiis naturalibus, tanta sit in finibus bonorum malorumque dissimilitudo.",
-//       },
-//       {
-//         title: "How to Make Dahi Vada at Home",
-//         blog_image: "featured-3.jpg",
-//         author_img: "author-4.jpg",
-//         category: "Cooking",
-//         content:
-//           "Aenean eget urna aliquet, viverra orci quis, aliquam erat. Ut rutrum quam quam, eu eleifend est blandit et. Vivamus suscipit ultrices venenatis. Aliquam massa ipsum, porta quis hendrerit at, varius sed leo. Curabitur convallis urna sit amet mi tempus posuere.",
-//       },
-//       {
-//         title: "Alternate Reality",
-//         blog_image: "featured-4.jpg",
-//         author_img: "author-5.jpg",
-//         category: "Science",
-//         content:
-//           "Non est igitur summum malum dolor. Tu autem inter haec tantam multitudinem hominum interiectam non vides nec laetantium nec dolentium. Nunc vero a primo quidem mirabiliter occulta natura est nec perspici nec cognosci potest.",
-//       },
-//     ]);
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+async function insertDummyData() {
+  try {
+    await Blog.insertMany([
+      {
+        title: "The Fairy Pools on the Isle of Skye – Scotland",
+        blog_image: "recommended-1.jpg",
+        author_img: "author-5.jpg",
+        category: "Travel",
+        content:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      },
+      {
+        title: "Marble Caverns of Carrera Lake – Chile",
+        blog_image: "recommended-5.jpg",
+        author_img: "author-6.jpg",
+        category: "Travel",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      },
+      {
+        title: "Northern Lights",
+        blog_image: "recommended-4.jpg",
+        author_img: "author-4.jpg",
+        category: "Travel",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      },
+      {
+        title: "Railay Beach",
+        blog_image: "recommended-3.jpg",
+        author_img: "author-1.jpg",
+        category: "Travel",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      },
+      {
+        title: "Rainbow mountains",
+        blog_image: "recommended-2.jpg",
+        author_img: "author-3.jpg",
+        category: "Travel",
+        content: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+      },
+    ]);
+  } catch (err) {
+    console.error(err);
+  }
+}
 // insertDummyData();
